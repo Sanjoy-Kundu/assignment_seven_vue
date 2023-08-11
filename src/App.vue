6acf122dc4dd8f10a843cebcb736556f9ed38af8
sanjoy <sanjoykundu187@gamil.com>
2023-08-12 01:31:44 +0600

    chart added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ImageCarousel from './components/ImageCarousel.vue';
+import ChatPart from './components/ChatPart.vue';
 </script>
 <template>
   <div class="container mx-auto px-4">
@@ -8,7 +9,18 @@ import ImageCarousel from './components/ImageCarousel.vue';
               <ImageCarousel/>
           </div>
     </div>
+    <div class="flex items-center justify-center h-screen mt-10">
+          <div class="cart">
+              <ChatPart/>
+          </div>
+    </div>
 
 </div>
 </template>
-<style></style>
+<style>
+.cart{
+  width: 400px;
+  height: 400px;
+  background-color: rgb(202, 142, 195);
+}
+</style>
---
+++ src/components/ChatPart.vue
@@ -0,0 +1,50 @@
+
+  
+  <script setup>
+  import {ref, onMounted} from 'vue';
+
+  let chart = null;
+  
+  const dataset = [
+	300, 50, 100
+  ]
+const data = {
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+const config = {
+  type: 'pie',
+  data: data,
+};
+
+onMounted(()=>{
+	const ctx = document.getElementById('chart');
+	chart = new Chart (ctx, config);
+})
+
+  </script>
+  
+  <template>
+	<div>
+  <canvas id="chart"></canvas>
+</div>
+  </template>
+
+
+  <style scoped>
+  </style>
+  
