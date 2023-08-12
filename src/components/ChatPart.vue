6efb8ec1dbf00d68048cda1fd729731b7c9152bc
sanjoy <sanjoykundu187@gamil.com>
2023-08-12 23:50:32 +0600

    finally done

--- src/components/ChatPart.vue
+++ src/components/ChatPart.vue
@@ -1,45 +1,50 @@
 <script setup>
 import {ref, onMounted, onBeforeUnmount } from 'vue';
 
-const newValue = ref(10);
+let chart = null;
+const newNumber = ref(0)
+const dataset = [9, 16, 7, 3, 14];
 
-let chart = null
-const dataset =  [300, 50, 100]
-
-const data = {
+  const data = {
   labels: [
     'Red',
-    'Blue',
-    'Yellow'
+    'Green',
+    'Yellow',
+    'Grey',
+    'Blue'
   ],
   datasets: [{
-    label: 'My First Dataset',
+    label: 'See Your Data Set',
     data:dataset,
     backgroundColor: [
       'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)'
-    ],
-    hoverOffset: 4
+      'rgb(75, 192, 192)',
+      'rgb(255, 205, 86)',
+      'rgb(201, 203, 207)',
+      'rgb(54, 162, 235)'
+    ]
   }]
 };
 
 const config = {
-  type: 'pie',
+  type: 'polarArea',
   data: data,
+  options: {}
 };
 
-onMounted(()=>{
-  const ctx = document.getElementById('pie_chart');
-  chart = new Chart(ctx, config);
 
+
+onMounted(()=>{
+  const ctx = document.getElementById('myChart');
+  chart = new Chart(ctx, config);
 })
 
-function update(){
-    dataset.push(newValue.value);
-    chart.data.datasets[0].data = dataset
-    chart.update();
+function updateChart(){
+  dataset.push(newNumber.value);
+  chart.data.datasets[0].data = dataset
+  chart.update()
 }
+
 
 onBeforeUnmount (()=>{
   alert('Before clear all');
@@ -49,13 +54,12 @@ onBeforeUnmount (()=>{
 
 <template>
   <div class="chart">
-        <canvas id="pie_chart"></canvas>
-        <div class="body">
-          <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="newValue">
-          <button class="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="update()">
-            Button
-          </button>
-        </div>
+    <h2 class="text-center">Wellcome to My chart</h2>
+    <canvas id="myChart"></canvas>
+      <div class="mt-10">
+        <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="newNumber">
+        <button class="bg-indigo-500 p-2 mt-3" @click="updateChart()">Apply</button>
+      </div>
   </div>
  
 </template>
@@ -68,16 +72,7 @@ onBeforeUnmount (()=>{
   height: 400px;
   background-color: rgb(202, 142, 195);
 margin-top: 20px;
-}
-.chart_section{
-  width: 400px;
-  height: 200px;
-  border: 1px solid black;
-}
-
-.body{
-    width: 300px;
-    height: 50px;
-    margin: 30px auto;
+border-radius: 10px;
+box-shadow: 2px 4px 3px black;
 }
 </style>
